0d393278d139417746eb7cfcdd70cbd107fae8cf
top <kritkrai.rak@bru.ac.th>
2018-08-16 04:02:26 +0700

    Initial commit

---
+++ src/main/java/com/kasikorn/demo/BankKasikornApplication.java
@@ -0,0 +1,12 @@
+package com.kasikorn.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankKasikornApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankKasikornApplication.class, args);
+	}
+}
