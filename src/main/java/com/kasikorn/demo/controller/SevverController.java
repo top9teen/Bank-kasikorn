5595322c4809f393fe566022a639873bd4a5eae9
top <35331078+top9teen@users.noreply.github.com>
2018-08-27 15:48:12 +0700

    ddfdsf

--- src/main/java/com/kasikorn/demo/controller/SevverController.java
+++ src/main/java/com/kasikorn/demo/controller/SevverController.java
@@ -6,25 +6,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kasikorn.demo.Bean.KasikornPriceBean;
 import com.kasikorn.demo.Bean.MiradoBean;
 import com.kasikorn.demo.Dao.KasikornServer;
 
-
-@CrossOrigin(origins="http://localhost:8080")
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 public class SevverController {
-@Autowired
-KasikornServer kasikornServer;
-	@RequestMapping("/kaprice")
-	public  KasikornPriceBean kars(@RequestBody MiradoBean miradoBean) throws SQLException{
-		KasikornPriceBean  ff = new KasikornPriceBean();
-		MiradoBean bean = new MiradoBean();
-String a =bean.getGroupType();
-String b = bean.getCarMake2();
-ff = kasikornServer.checkpriceKa(a, b);
+	@Autowired
+	KasikornServer kasikornServer;
+
+	@RequestMapping(value="/kaprice" , method = RequestMethod.POST)
+	public KasikornPriceBean kars(@RequestBody MiradoBean miradoBean) throws SQLException {
+		KasikornPriceBean ff = new KasikornPriceBean();
+//		MiradoBean bean = new MiradoBean();
+		String a = miradoBean.getGroupType();
+		String b = miradoBean.getCarMake2();
+		ff = kasikornServer.checkpriceKa(a, b);
 		return ff;
 	}
 }
